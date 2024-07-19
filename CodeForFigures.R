8f627794ccf53e9d075ad511a4fc97a88c0e8410
anonymous2738 <151526847+anonymous2738@users.noreply.github.com>
2024-07-19 14:09:24 +0100

    Update CodeForFigures.R

--- CodeForFigures.R
+++ CodeForFigures.R
@@ -1700,206 +1700,217 @@ figure7<-function(max_iter = 6000, burn_in = 1000, num_of_datasets = 100){
   }
   
   AddiVortes_Algorithm<-function(y,x,m,max_iter,burn_in,nu,q,k,var,Omega,lambda_rate,YTest,XTest,IntialSigma = "Linear"){
-    
-    #Scaling x and y
-    yScaled=(y-(max(y)+min(y))/2)/(max(y)-min(y))
-    xScaled=x;
-    for (i in 1:length(x[1,])){
-      xScaled[,i]=(x[,i]-(max(x[,i])+min(x[,i]))/2)/(max(x[,i])-min(x[,i]));
-    }
-    
-    
-    for (i in 1:length(XTest[1,])){
-      XTest[,i]=(XTest[,i]-(max(x[,i])+min(x[,i]))/2)/(max(x[,i])-min(x[,i]));
-    }
-    
-    #Initialize:
-    #Prediction Set (A list of vectors with the output values for each tessellation), 
-    #Dimension set (A list of vectors with the covariates included in the tessellaions);
-    #and Tessellation Set (A list of matrices that give the coordinates of the centers in the tessellations)
-    
-    Pred<-rep(list(matrix(mean(yScaled)/m)),m)
-    Dim=vector(length = m)
-    Tess=vector(length = m)
-    for (i in 1:m){
-      Dim[i]<-list(sample(1:length(x[1,]), 1))
-      Tess[i]<-(list(matrix(rnorm(1,0,var))))
-    }
-    
-    #Prepare some variables used in the backfitting algorithm
-    SumOfAllTess=rep(mean(yScaled),length(yScaled))
-    SigmaSquaredMu=(0.5/(k*sqrt(m)))^2
-    LastTessPred=matrix
-    
-    #Matrices that will hold the samples from the poseterior distribution for the training samples and test samples.
-    PredictionMatrix<-array(dim=c(length(y),(max_iter-burn_in)))
-    TestMatrix<-array(dim=c(length(YTest),(max_iter-burn_in)))
-    
-    #finding lambda
-    if (IntialSigma=="Naive"){ # Usually used if p is greater then n. Uses Standard deviation of y to predict sigma.
-      SigmaSquaredHat=var(yScaled)
-    }
-    else{  # Default method using residual standard deviation from a least-squared linear regression of y, to predict sigma.
-      MultiLinear<-lm(yScaled ~ xScaled)
-      SigmaSquaredHat=sum(MultiLinear$residuals^2)/(length(yScaled)-length(xScaled[1,])-1)
-    }
-    
-    #Find lambda
-    lambda=1;
-    lambda <- optim(par = 1,
-                    fitting_function,
-                    method = "Brent",
-                    lower = 0.001,
-                    upper = 100,
-                    q=q , nu=nu, sigmaSquared_hat=SigmaSquaredHat)$par
   
-    for (i in 1:max_iter){
+  #Scaling x and y
+  yScaled=(y-(max(y)+min(y))/2)/(max(y)-min(y))
+  xScaled=x;
+  for (i in 1:length(x[1,])){
+    xScaled[,i]=(x[,i]-(max(x[,i])+min(x[,i]))/2)/(max(x[,i])-min(x[,i]));
+  }
+  
+  
+  for (i in 1:length(XTest[1,])){
+    XTest[,i]=(XTest[,i]-(max(x[,i])+min(x[,i]))/2)/(max(x[,i])-min(x[,i]));
+  }
+  
+  #Initialize:
+  #Prediction Set (A list of vectors with the output values for each tessellation), 
+  #Dimension set (A list of vectors with the covariates included in the tessellaions);
+  #and Tessellation Set (A list of matrices that give the coordinates of the centers in the tessellations)
+  
+  Pred<-rep(list(matrix(mean(yScaled)/m)),m)
+  Dim=vector(length = m)
+  Tess=vector(length = m)
+  for (i in 1:m){
+    Dim[i]<-list(sample(1:length(x[1,]), 1))
+    Tess[i]<-(list(matrix(rnorm(1,0,var))))
+  }
+  
+  #Prepare some variables used in the backfitting algorithm
+  SumOfAllTess=rep(mean(yScaled),length(yScaled))
+  SigmaSquaredMu=(0.5/(k*sqrt(m)))^2
+  LastTessPred=matrix
+  
+  #Matrices that will hold the samples from the poseterior distribution for the training samples and test samples.
+  PredictionMatrix<-array(dim=c(length(y),(max_iter-burn_in)))
+  TestMatrix<-array(dim=c(length(YTest),(max_iter-burn_in)))
+  
+  #finding lambda
+  if (IntialSigma=="Naive"){ # Usually used if p is greater then n. Uses Standard deviation of y to predict sigma.
+    SigmaSquaredHat=var(yScaled)
+  }
+  else{  # Default method using residual standard deviation from a least-squared linear regression of y, to predict sigma.
+    MultiLinear<-lm(yScaled ~ xScaled)
+    SigmaSquaredHat=sum(MultiLinear$residuals^2)/(length(yScaled)-length(xScaled[1,])-1)
+  }
+  
+  #Find lambda
+  lambda=1;
+  lambda <- optim(par = 1,
+                  fitting_function,
+                  method = "Brent",
+                  lower = 0.001,
+                  upper = 100,
+                  q=q , nu=nu, sigmaSquared_hat=SigmaSquaredHat)$par
+  
+  for (i in 1:max_iter){
+    
+    #Sample Sigma squared using all tessellations to predict the outcome variables
+    SigmaSquared=SigmaSquaredCalculation(yScaled,nu,lambda,SumOfAllTess)
+    
+    
+    for (j in 1:m){
+      NewTessOutput<-NewTess(xScaled,j,Tess,Dim,var) #Propose new Tessellation 
+      TessStar<-NewTessOutput[[1]]  
+      DimStar<-NewTessOutput[[2]]
+      Modification<-NewTessOutput[[3]]
       
-      #Sample Sigma squared using all tessellations to predict the outcome variables
-      SigmaSquared=SigmaSquaredCalculation(yScaled,nu,lambda,SumOfAllTess)
+      ResidualsOutput<-CalculateResiduals(yScaled,xScaled,j,SumOfAllTess,Tess,Dim,Pred,TessStar,DimStar,LastTessPred) #Calculate the n-vector of partial residuals derived from a fitting process that excludes the jth tessellation and the number of observations in each cell.
+      R_ijOld<-ResidualsOutput[[1]]   #Old and New refer to the original and proposed tessellations
+      n_ijOld<-ResidualsOutput[[2]]
+      R_ijNew<-ResidualsOutput[[3]]
+      n_ijNew<-ResidualsOutput[[4]]
+      SumOfAllTess<-ResidualsOutput[[5]] #Keeps track of the prediction for all tessellations to help sample sigma squared.
+      IndexesStar<-ResidualsOutput[[6]] #Gives the row of each observation for the cell it falls in for the proposed tessellation.
+      Indexes<-ResidualsOutput[[7]]  #Gives the row of each observation for the cell it falls in for the original tessellation.
       
-      
-      for (j in 1:m){
-        NewTessOutput<-NewTess(xScaled,j,Tess,Dim,var) #Propose new Tessellation 
-        TessStar<-NewTessOutput[[1]]  
-        DimStar<-NewTessOutput[[2]]
-        Modification<-NewTessOutput[[3]]
+      if (!any(n_ijNew==0)){ #automatically rejects proposed tessellation if there exists a cell with no observations in.
+        LOGAcceptenceProb=AlphaCalculation(xScaled,TessStar,DimStar,j,R_ijOld,n_ijOld,R_ijNew,n_ijNew,SigmaSquared,Modification,SigmaSquaredMu,Omega,lambda_rate) #Gives the log of the acceptence probability.
         
-        ResidualsOutput<-CalculateResiduals(yScaled,xScaled,j,SumOfAllTess,Tess,Dim,Pred,TessStar,DimStar,LastTessPred) #Calculate the n-vector of partial residuals derived from a fitting process that excludes the jth tessellation and the number of observations in each cell.
-        R_ijOld<-ResidualsOutput[[1]]   #Old and New refer to the original and proposed tessellations
-        n_ijOld<-ResidualsOutput[[2]]
-        R_ijNew<-ResidualsOutput[[3]]
-        n_ijNew<-ResidualsOutput[[4]]
-        SumOfAllTess<-ResidualsOutput[[5]] #Keeps track of the prediction for all tessellations to help sample sigma squared.
-        IndexesStar<-ResidualsOutput[[6]] #Gives the row of each observation for the cell it falls in for the proposed tessellation.
-        Indexes<-ResidualsOutput[[7]]  #Gives the row of each observation for the cell it falls in for the original tessellation.
-        
-        if (!any(n_ijNew==0)){ #automatically rejects proposed tessellation if there exists a cell with no observations in.
-          LOGAcceptenceProb=AlphaCalculation(xScaled,TessStar,DimStar,j,R_ijOld,n_ijOld,R_ijNew,n_ijNew,SigmaSquared,Modification,SigmaSquaredMu,Omega,lambda_rate) #Gives the log of the acceptence probability.
-          
-          if (log(runif(n=1, min=0, max=1))<LOGAcceptenceProb){ #Accepts the proposed tessellation is accepted then calculates the new output values for the new tessellation. 
-            Tess=TessStar
-            Dim=DimStar
-            Pred[[j]]=NewPredSet(j,TessStar,R_ijNew,n_ijNew,SigmaSquaredMu,SigmaSquared)
-            LastTessPred=Pred[[j]][IndexesStar]
-          }
-          else { #Rejects the proposed tesellation then calculates new output values for the original tessellation.
-            Pred[[j]]=NewPredSet(j,Tess,R_ijOld,n_ijOld,SigmaSquaredMu,SigmaSquared);
-            LastTessPred=Pred[[j]][Indexes];
-          }
+        if (log(runif(n=1, min=0, max=1))<LOGAcceptenceProb){ #Accepts the proposed tessellation is accepted then calculates the new output values for the new tessellation. 
+          Tess=TessStar
+          Dim=DimStar
+          Pred[[j]]=NewPredSet(j,TessStar,R_ijNew,n_ijNew,SigmaSquaredMu,SigmaSquared)
+          LastTessPred=Pred[[j]][IndexesStar]
         }
-        else{ #Rejects the proposed tesellation then calculates new output values for the original tessellation.
+        else { #Rejects the proposed tesellation then calculates new output values for the original tessellation.
           Pred[[j]]=NewPredSet(j,Tess,R_ijOld,n_ijOld,SigmaSquaredMu,SigmaSquared);
           LastTessPred=Pred[[j]][Indexes];
         }
-        if (j==m){ #If j equals m then adds the last tessellation output values to give a prediction.
-          SumOfAllTess=SumOfAllTess+LastTessPred;
-        }
       }
-      
-      if (i>burn_in){ #vectors that hold the predictions for each iteration after burn in.
-        PredictionMatrix[,(i-burn_in)]=SumOfAllTess;
-        TestMatrix[,(i-burn_in)]=TestPrediction(XTest,m,Tess,Dim,Pred);
+      else{ #Rejects the proposed tesellation then calculates new output values for the original tessellation.
+        Pred[[j]]=NewPredSet(j,Tess,R_ijOld,n_ijOld,SigmaSquaredMu,SigmaSquared);
+        LastTessPred=Pred[[j]][Indexes];
       }
-      if (i %% 100 == 0){
-        cat(sprintf("Iteration %d out of %d", i, max_iter), "\n")
+      if (j==m){ #If j equals m then adds the last tessellation output values to give a prediction.
+        SumOfAllTess=SumOfAllTess+LastTessPred;
       }
     }
     
-    #finding the mean of the predition over the iterations and then unscaling the predictions.
-    mean_yhat=(rowSums(PredictionMatrix)/(max_iter-burn_in))*(max(y)-min(y))+((max(y)+min(y))/2)
-    mean_yhat_Test=(rowSums(TestMatrix)/(max_iter-burn_in))*(max(y)-min(y))+((max(y)+min(y))/2)
-    
-    
-    return( #Returns the RMSE value for the test samples.
-      data.frame(
-        RMSE = sqrt(mean((YTest-mean_yhat_Test)^2)),
-        mean_yhat_Test
-      )
+    if (i>burn_in){ #vectors that hold the predictions for each iteration after burn in.
+      PredictionMatrix[,(i-burn_in)]=SumOfAllTess;
+      TestMatrix[,(i-burn_in)]=TestPrediction(XTest,m,Tess,Dim,Pred);
+    }
+    if (i %% 100 == 0){
+      cat(sprintf("Iteration %d out of %d", i, max_iter), "\n")
+    }
+  }
+  
+  #finding the mean of the predition over the iterations and then unscaling the predictions.
+  mean_yhat=(rowSums(PredictionMatrix)/(max_iter-burn_in))*(max(y)-min(y))+((max(y)+min(y))/2)
+  mean_yhat_Test=(rowSums(TestMatrix)/(max_iter-burn_in))*(max(y)-min(y))+((max(y)+min(y))/2)
+  
+  
+  return( #Returns the RMSE value for the test samples.
+    data.frame(
+      RMSE = sqrt(mean((YTest-mean_yhat_Test)^2)),
+      mean_yhat_Test
     )
-  }
-  
-    
-  f = function(x){
-    10*sin(pi*x[,1]*x[,2]) + 20*(x[,3]-0.5)^2+10*x[,4]+5*x[,5]
-  }
-  
-  sigma = 1  #y = f(x) + sigma*z , z~N(0,1)
-  n = 200      #number of observations
-  set.seed(9)
-  X=matrix(runif(n*10),n,10) #10 variables, only first 5 matter
-  Ey = f(X)
-  Y=Ey+sigma*rnorm(n)
-  
-  n=length(Y)
-  TrainSet=sort(sample.int(n,3*n/6))
-  TestSet=1:n
-  TestSet=TestSet[! TestSet %in% TrainSet]
-  
-  hyperparametersAddiVortesRobust <- matrix(
-    c(10, 6,3,
-      0.75,0.85,0.95,
-      4,3,2,
-      1.5,0.8,0.8,
-      3,3,1,
-      25,25,5), ncol=6
   )
-  
-  
-  rmse_values<-numeric(3)
-  RobustnessValues<-array(dim=c(3,num_of_datasets,8))
-  FullRobustnessValues<-array(dim=c(3,num_of_datasets,8))
-  
-  num_cores <- 10  # Specify the number of cores you want to use
-  cl <- makeCluster(num_cores)
-  registerDoParallel(cl)
-  
-  for (m in c(1,10,50,100,200,300,400,500)){
-    t<-which(m==c(1,10,50,100,200,300,400,500))
-    RobustnessValues[,,t]<-foreach (i = 1:num_of_datasets,.combine=cbind) %dopar% {
-      library('invgamma')
-      library('FNN')
-      library('expint')
-      
-      for (i in 1:nrow(hyperparametersAddiVortesRobust)){
-        params<-hyperparametersAddiVortesRobust[i,]
-        nu <- params[1]
-        q<- params[2]
-        k<- params[3]
-        sd<- params[4]
-        omega<- params[5]
-        lambda<- params[6]
-        
-        rmse_values[i] <- AddiVortes_Algorithm(Y[TrainSet],as.matrix(X[TrainSet,]),m,max_iter,burn_in,nu,q,k,sd,omega,lambda,f(X[TestSet,]),as.matrix(X[TestSet,]))$RMSE
-      }
+}
+
+
+f = function(x){
+  10*sin(pi*x[,1]*x[,2]) + 20*(x[,3]-0.5)^2+10*x[,4]+5*x[,5]
+}
+
+sigma = 1  #y = f(x) + sigma*z , z~N(0,1)
+n = 300      #number of observations
+set.seed(24)
+X=matrix(runif(n*10),n,10) #10 variables, only first 5 matter
+Ey = f(X)
+Y=Ey+sigma*rnorm(n)
+
+n=length(Y)
+TrainSet=sort(sample.int(n,3*n/6))
+TestSet=1:n
+TestSet=TestSet[! TestSet %in% TrainSet]
+
+
+hyperparametersAddiVortesRobust <- matrix(
+  c(6, 6,10,
+    0.95,0.85,0.75,
+    1,3,3,
+    1.5,0.8,0.8,
+    3,3,1,
+    25,25,5), ncol=6
+)
+
+
+
+rmse_values<-numeric(3)
+RobustnessValues<-array(dim=c(3,num_of_datasets,8))
+FullRobustnessValues<-array(dim=c(3,num_of_datasets,8))
+
+num_cores <- 10  # Specify the number of cores you want to use
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+for (m in c(1,10,50,100,200,300,400,500)){
+  t<-which(m==c(1,10,50,100,200,300,400,500))
+  RobustnessValues[,,t]<-foreach (i = 1:num_of_datasets,.combine=cbind) %dopar% {
+    library('invgamma')
+    library('FNN')
+    library('expint')
     
-      return(rmse_values)
+    for (i in 1:nrow(hyperparametersAddiVortesRobust)){
+      params<-hyperparametersAddiVortesRobust[i,]
+      nu <- params[1]
+      q<- params[2]
+      k<- params[3]
+      sd<- params[4]
+      omega<- params[5]
+      lambda<- params[6]
+      
+      rmse_values[i] <- AddiVortes_Algorithm(Y[TrainSet],as.matrix(X[TrainSet,]),m,1200,200,nu,q,k,sd,omega,lambda,f(X[TestSet,]),as.matrix(X[TestSet,]))$RMSE
     }
+    
+    return(rmse_values)
   }
-  
-  
-  stopCluster(cl)
-  
-  FullMeanValues<-matrix(nrow = 3,ncol = 8)
-  FullLowerquantileValues<-matrix(nrow = 3,ncol = 8)
-  FullUpperquantileValues<-matrix(nrow = 3,ncol = 8)
-  
-  for (i in 1:8){
-    FullMeanValues[,i]<-rowMeans(RobustnessValues[,,i])
-    FullLowerquantileValues[,i]<- apply(RobustnessValues[,,i], 1, quantile, probs = c(0.05, 0.95))[1,]
-    FullUpperquantileValues[,i]<- apply(RobustnessValues[,,i], 1, quantile, probs = c(0.05, 0.95))[2,]
-  }
-  
-  
-  plotCI(c(1,10,50,100,200,300,400,500),FullMeanValues[1,],FullUpperquantileValues[1,]-FullMeanValues[1,],FullMeanValues[1,]-FullLowerquantileValues[1,], sfrac=0, scol = 'red',pch = 16,cex = 0.6,col = 'red',xlim = c(1,500),ylim = c(1,5),lwd=2,xlab = "Number of Tessellations, m", ylab = "RMSE" )
-  
-  points(c(1-5,10-5,50-5,100-5,200-5,300-5,400-5,500-5),FullMeanValues[2,], pch = 16,cex = 0.6,col = 'blue')
-  arrows( c(1-5,10-5,50-5,100-5,200-5,300-5,400-5,500-5),FullUpperquantileValues[2,], c(1-5,10-5,50-5,100-5,200-5,300-5,400-5,500-5),FullLowerquantileValues[2,], angle = 90, code = 3, length = 0, col = "blue",lwd= 2)
-  
-  points(c(1+5,10+5,50+5,100+5,200+5,300+5,400+5,500+5),FullMeanValues[3,], pch = 16,cex = 0.6, col = 'green')
-  arrows( c(1+5,10+5,50+5,100+5,200+5,300+5,400+5,500+5),FullUpperquantileValues[3,], c(1+5,10+5,50+5,100+5,200+5,300+5,400+5,500+5),FullLowerquantileValues[3,], angle = 90, code = 3, length = 0, col = "green",lwd = 2)
-  
-  legend("topright", legend = c("Aggressive","Default","Conservative"), col = c('red','blue','green'), lwd = 2,inset = 0.1,) 
+  print(t)
+}
+
+
+stopCluster(cl)
+
+FullMeanValues<-matrix(nrow = 3,ncol = 8)
+FullLowerquantileValues<-matrix(nrow = 3,ncol = 8)
+FullUpperquantileValues<-matrix(nrow = 3,ncol = 8)
+
+for (i in 1:8){
+  FullMeanValues[,i]<-rowMeans(RobustnessValues[,,i])
+  FullLowerquantileValues[,i]<- apply(RobustnessValues[,,i], 1, quantile, probs = c(0.05, 0.95))[1,]
+  FullUpperquantileValues[,i]<- apply(RobustnessValues[,,i], 1, quantile, probs = c(0.05, 0.95))[2,]
+}
+
+
+plotCI(c(1,10,50,100,200,300,400,500),FullMeanValues[1,],FullUpperquantileValues[1,]-FullMeanValues[1,],FullMeanValues[1,]-FullLowerquantileValues[1,], sfrac=0, scol = 'red',pch = 16,cex = 0.75,col = 'red',xlim = c(1,500),ylim = c(1,5),lwd=2,xlab = "Number of Tessellations, m", ylab = "RMSE" )
+
+points(c(1-5,10-5,50-5,100-5,200-5,300-5,400-5,500-5),FullMeanValues[2,], pch = 15,cex = 0.75,col = 'blue')
+arrows( c(1-5,10-5,50-5,100-5,200-5,300-5,400-5,500-5),FullUpperquantileValues[2,], c(1-5,10-5,50-5,100-5,200-5,300-5,400-5,500-5),FullLowerquantileValues[2,], angle = 90, code = 3, length = 0, col = "blue",lwd= 2)
+
+points(c(1+5,10+5,50+5,100+5,200+5,300+5,400+5,500+5),FullMeanValues[3,], pch = 17,cex =0.75, col = 'darkgreen')
+arrows( c(1+5,10+5,50+5,100+5,200+5,300+5,400+5,500+5),FullUpperquantileValues[3,], c(1+5,10+5,50+5,100+5,200+5,300+5,400+5,500+5),FullLowerquantileValues[3,], angle = 90, code = 3, length = 0, col = "darkgreen",lwd = 2)
+
+legend(
+  "topright",
+  legend = c("Aggressive", "Default", "Conservative"),
+  col = c('red', 'blue', 'darkgreen'),
+  pch = c(16, 15, 17),
+  lwd = 2,
+  inset = 0.1,bty = "n"
+)
+
 
 }
 
